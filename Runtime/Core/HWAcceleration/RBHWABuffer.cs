1a684240ac6737d333ad3219c39c93ecd7ced289
JA8551 <122510325+JA8551@users.noreply.github.com>
2025-01-04 03:50:22 +0900

    修正

---
+++ Runtime/Core/HWAcceleration/RBHWABuffer.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace RBPhys
+{
+    public class RBHWABuffer<T> : IDisposable
+    {
+        GraphicsBuffer _graphicsBuffer;
+        int _count;
+
+        int _stride;
+
+        public int Count { get { return _count; } }
+
+        public RBHWABuffer(GraphicsBuffer.Target target, int count)
+        {
+            _count = count;
+            _graphicsBuffer = new GraphicsBuffer(target, count, Marshal.SizeOf(typeof(T)));
+        }
+
+        public RBHWABuffer(int count)
+        {
+            _count = count;
+            _stride = Marshal.SizeOf(typeof(T));
+            _graphicsBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, count, _stride);
+        }
+
+        public GraphicsBuffer GetGraphicsBuffer()
+        {
+            return _graphicsBuffer;
+        }
+
+        public void SetData(T[] data)
+        {
+            _graphicsBuffer.SetData(data);
+        }
+
+        public void GetData(T[] data)
+        {
+            _graphicsBuffer.GetData(data);
+        }
+
+        public T[] GetData()
+        {
+            T[] data = new T[_count];
+            _graphicsBuffer.GetData(data);
+
+            return data;
+        }
+
+        public void Dispose()
+        {
+            _graphicsBuffer.Dispose();
+            _count = -1;
+        }
+    }
+}
